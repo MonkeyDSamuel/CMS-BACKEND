bb8aa4cfbd3f53be6b02349c8003f80b58c75421
MonkeyDSamuel <samueleternityunofficial@gmail.com>
2025-07-28 12:24:40 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 8002;
+
+app.use(cors());
+app.use(express.json());
+
+//connect to db and start the server
+connectDB().then(() => {
+    app.listen(PORT,() => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
