de1ea27d23a2f72f4fba5039694f944745cf4f7b
MonkeyDSamuel <samueleternityunofficial@gmail.com>
2025-07-29 17:09:23 +0530

    Doctor modules Added

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
+const doctorRoutes = require('./routes/doctorRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 8002;
 
 app.use(cors());
 app.use(express.json());
+app.use(doctorRoutes);
 
 //connect to db and start the server
 connectDB().then(() => {
